797b78e5f417e19180d9a4b79b89ff087790cd33
Lumi (Wonderland) <lumi@wonderland.xyz>
2025-06-05 16:13:13 -0300

    chore: initial commit

---
+++ sidebars.ts
@@ -0,0 +1,24 @@
+import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+
+const sidebars: SidebarsConfig = {
+  tutorialSidebar: [
+    'welcome',
+    // {
+    //   type: 'category',
+    //   label: 'Stack',
+    //   items: [
+    //     'stack/overview',
+    //     'stack/previous-knowledge',
+    //     'stack/op-stack',
+    //     'stack/deposit-flow',
+    //     'stack/withdrawal-flow',
+    //     'stack/evm-vs-op',
+    //     'stack/precompiles-and-predeploys',
+    //     'stack/preinstalls',
+    //     'stack/bye',
+    //   ],
+    // },
+  ],
+};
+
+export default sidebars;
